7f978476ea0d7a46246724b7ae115729aa19a219
Matyas Gombos <gombosmm@gmail.com>
2022-11-21 16:40:28 -0500

    Add dependencies and fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { Reducer } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
+import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import App from './App';
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const store = configureStore(Reducer, applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,10 +14,5 @@ root.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Provider>
+  </Provider>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
