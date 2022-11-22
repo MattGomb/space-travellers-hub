b02a5454804c225923c711040b131617e19332f9
Matyas Gombos <gombosmm@gmail.com>
2022-11-22 11:59:44 -0500

    Fetch API data, and create slice

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,4 +1,22 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+const url = 'https://api.spacexdata.com/v3/missions';
+
+export const fetchMissions = createAsyncThunk(
+  'missions/fetchMissions',
+  async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const missions = data.map((mission) => ({
+      reserved: false,
+      id: mission.mission_id,
+      name: mission.mission_name,
+      description: mission.description,
+    }));
+    return missions;
+  },
+);
 
 const initialState = [];
 
@@ -6,6 +24,15 @@ const missionSlice = createSlice({
   name: 'mission',
   initialState,
   reducers: {
+    reserveMission: (state, action) => state.map((mission) => {
+      if (mission.id === action.payload) {
+        return { ...mission, reserved: !mission.reserved };
+      }
+      return mission;
+    }),
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchMissions.fulfilled, (state, action) => action.payload);
   },
 });
 
