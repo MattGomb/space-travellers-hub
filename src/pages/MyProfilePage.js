f82b4944e337d817bf2added0e588228ae3565fd
Matyas Gombos <gombosmm@gmail.com>
2022-11-23 13:55:44 -0500

    Add empty table messages on profile

--- src/pages/MyProfilePage.js
+++ src/pages/MyProfilePage.js
@@ -3,10 +3,11 @@ import { useSelector } from 'react-redux';
 import style from '../styles/ProfilePage.module.css';
 
 const MyProfile = () => {
-  const JoinedMissionsList = () => {
-    const missions = useSelector((state) => state.missions);
-    const joinedMissions = missions.filter((mission) => mission.reserved);
-    return (
+  const missions = useSelector((state) => state.missions);
+  const joinedMissions = missions.filter((mission) => mission.reserved);
+
+  const JoinedMissionsList = () => (
+    <section>
       <table className={style.reservedMTable}>
         <tbody>
           {joinedMissions.map((mission) => (
@@ -16,13 +17,17 @@ const MyProfile = () => {
           ))}
         </tbody>
       </table>
-    );
-  };
+      {joinedMissions.length === 0 && (
+        <p className={style.noMissions}>NO MISSIONS JOINED</p>
+      )}
+    </section>
+  );
 
-  const ReservedRocketsList = () => {
-    const rockets = useSelector((state) => state.rockets);
-    const reservedRockets = rockets.filter((rocket) => rocket.reserved);
-    return (
+  const rockets = useSelector((state) => state.rockets);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved);
+
+  const ReservedRocketsList = () => (
+    <section>
       <table className={style.reservedRTable}>
         <tbody>
           {reservedRockets.map((rocket) => (
@@ -32,17 +37,20 @@ const MyProfile = () => {
           ))}
         </tbody>
       </table>
-    );
-  };
+      {reservedRockets.length === 0 && (
+        <p className={style.noRockets}>NO ROCKETS RESERVED</p>
+      )}
+    </section>
+  );
 
   return (
     <section className={style.profile}>
       <div className={style.profileContainer}>
-        <div className={`${style.reservedMissions} ${style.column}`}>
+        <div className={`${style.reservedMissions} ${style.column} ${joinedMissions.length === 0 && style.columnB}`}>
           <h2>My Mission</h2>
           <JoinedMissionsList />
         </div>
-        <div className={`${style.reservedRockets} ${style.column}`}>
+        <div className={`${style.reservedRockets} ${style.column} ${reservedRockets.length === 0 && style.columnB}`}>
           <h2>My Rockets</h2>
           <ReservedRocketsList />
         </div>
