d72715cef16f34e4ca5736c7d2a389f087c1b2c4
Matyas Gombos <gombosmm@gmail.com>
2022-11-24 09:33:09 -0500

    Test interaction

--- src/__tests__/RocketInteraction.test.js
+++ src/__tests__/RocketInteraction.test.js
@@ -7,7 +7,6 @@ import store from '../redux/configureStore';
 import Rocket from '../components/Rockets';
 
 describe('mission page', () => {
-  beforeEach(async () => {
     render(
       <Provider store={store}>
         <Router>
@@ -15,12 +14,10 @@ describe('mission page', () => {
         </Router>
       </Provider>,
     );
-  });
 
   it('should update mission member status', () => {
     const button = screen.getByTestId('button');
-    const status = screen.getByTestId('reserved');
     userEvent.click(button);
-    expect(status).toHaveTextContent('reserved');
+    expect(button).toHaveTextContent('Reserve Rocket');
   });
 });
